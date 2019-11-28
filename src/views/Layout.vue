d7990b2d473de192ad8a517c227e3c50e3fb123a
zyx <zhou@qq.com>
2019-11-28 20:02:29 +0800

    项目初始化

---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<!-- 公用布局 -->
+<template>
+  <div class="container">
+    <!-- 顶部导航 -->
+
+    <!-- 底部导航 -->
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+</style>
